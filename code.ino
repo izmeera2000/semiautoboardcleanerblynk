95c07f35530d73110e5568880ec8cf1f3fa0ecbc
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2022-06-05 09:45:37 +0800

    Update code.ino

--- code.ino
+++ code.ino
@@ -62,151 +62,33 @@ BLYNK_WRITE(V0)
       pos1--;
 
       delay(1);                       // waits 15 ms for the servo to reach the position
-      //      Serial.println("ISAP KOTE");
     }
   }
 }
 
 
 
-BLYNK_WRITE(V1)
+BLYNK_WRITE(V13)
 {
   pinValuex = param[0].asInt();
-  pinValuey = param[1].asInt();
+
+  //
+  //  Serial.print(pinValuex);
+  //  Serial.println("x");
+  //
+  //  Serial.print(pinValuey);
+  //  Serial.println("y");
 
 
-  Serial.print(pinValuex);
-  Serial.println("x");
-
-  Serial.print(pinValuey);
-  Serial.println("y");
-
-  if (pinValuex > 128 &&  pinValuey > 128)
-  {
-    digitalWrite(xdir, HIGH);
-    digitalWrite(ydir, HIGH );
-    for (x = 0; x < 800; x++)
-    {
-      digitalWrite(xstp, HIGH);
-      digitalWrite(ystp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-  }
-  else if (pinValuex > 128 && pinValuey < 128) {
-    digitalWrite(xdir, HIGH);
-    digitalWrite(ydir, LOW);
-    for (x = 0; x < 800; x++)
-    {
-      digitalWrite(xstp, HIGH);
-      digitalWrite(ystp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-  }
-  else if (pinValuex < 128 && pinValuey > 128) {
-    digitalWrite(ydir, HIGH);
-    digitalWrite(xdir, LOW);
-    for (x = 0; x < 800; x++)
-    {
-      digitalWrite(xstp, HIGH);
-      digitalWrite(ystp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-  }
-  else if (pinValuex < 128 && pinValuey < 128) {
-    digitalWrite(ydir, LOW);
-    digitalWrite(xdir, LOW);
-    for (x = 0; x < 800; x++)
-    {
-      digitalWrite(xstp, HIGH);
-      digitalWrite(ystp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-
-  }
-  else if (pinValuex == 128 && pinValuey < 128) {
-    digitalWrite(ydir, LOW);
-    //    digitalWrite(xdir, LOW);
-    for (x = 0; x < 800; x++)
-    {
-      //      digitalWrite(xstp, HIGH);
-      digitalWrite(ystp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-
-  }
-  else if (pinValuex == 128 && pinValuey > 128) {
-    digitalWrite(ydir, HIGH);
-    //    digitalWrite(xdir, LOW);
-    for (x = 0; x < 800; x++)
-    {
-      //      digitalWrite(xstp, HIGH);
-      digitalWrite(ystp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-
-  }
-  else if (pinValuex > 128 && pinValuey == 128) {
-    //    digitalWrite(ydir, HIGH);
-    digitalWrite(xdir, HIGH);
-    for (x = 0; x < 800; x++)
-    {
-      //      digitalWrite(xstp, HIGH);
-      digitalWrite(xstp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-
-  }
-  else if (pinValuex < 128 && pinValuey == 128) {
-    //    digitalWrite(ydir, HIGH);
-    digitalWrite(xdir, LOW);
-    for (x = 0; x < 800; x++)
-    {
-      //      digitalWrite(xstp, HIGH);
-      digitalWrite(xstp, HIGH);
-      delay(1);
-      digitalWrite(xstp, LOW);
-      digitalWrite(ystp, LOW);
-      delay(1);
-    }
-
-  }
-  else
-  { digitalWrite(ydir, LOW);
-    digitalWrite(xdir, LOW);
-    digitalWrite(xstp, LOW);
-    digitalWrite(ystp, LOW);
-    delay(100);
-  }
-  delay(100);
 
 
 
 }
-//BLYNK_WRITE(V14)
-//{
-//
-//}
+BLYNK_WRITE(V14)
+{
+  pinValuey = param[0].asInt();
+
+}
 void setup()
 {
   // Debug console
@@ -232,7 +114,126 @@ void setup()
 void loop()
 {
   Blynk.run();
-  //  Blynk.syncVirtual(V1);
+  delay(100);
+
+  if (pinValuex > 0 &&  pinValuey > 0)
+  {
+    digitalWrite(xdir, HIGH);
+    digitalWrite(ydir, HIGH );
+    for (x = 0; x < 800; x++)
+    {
+      digitalWrite(xstp, HIGH);
+      digitalWrite(ystp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+  }
+  else if (pinValuex > 0 && pinValuey < 0) {
+    digitalWrite(xdir, HIGH);
+    digitalWrite(ydir, LOW);
+    for (x = 0; x < 800; x++)
+    {
+      digitalWrite(xstp, HIGH);
+      digitalWrite(ystp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+  }
+  else if (pinValuex < 0 && pinValuey > 0) {
+    digitalWrite(ydir, HIGH);
+    digitalWrite(xdir, LOW);
+    for (x = 0; x < 800; x++)
+    {
+      digitalWrite(xstp, HIGH);
+      digitalWrite(ystp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+  }
+  else if (pinValuex < 0 && pinValuey < 0) {
+    digitalWrite(ydir, LOW);
+    digitalWrite(xdir, LOW);
+    for (x = 0; x < 800; x++)
+    {
+      digitalWrite(xstp, HIGH);
+      digitalWrite(ystp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+
+  }
+  else if (pinValuex == 0 && pinValuey < 0) {
+    digitalWrite(ydir, LOW);
+    //    digitalWrite(xdir, LOW);
+    for (x = 0; x < 800; x++)
+    {
+      //      digitalWrite(xstp, HIGH);
+      digitalWrite(ystp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+
+  }
+  else if (pinValuex == 0 && pinValuey > 0) {
+    digitalWrite(ydir, HIGH);
+    //    digitalWrite(xdir, LOW);
+    for (x = 0; x < 800; x++)
+    {
+      //      digitalWrite(xstp, HIGH);
+      digitalWrite(ystp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+
+  }
+  else if (pinValuex > 0 && pinValuey == 0) {
+    //    digitalWrite(ydir, HIGH);
+    digitalWrite(xdir, HIGH);
+    for (x = 0; x < 800; x++)
+    {
+      //      digitalWrite(xstp, HIGH);
+      digitalWrite(xstp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+
+  }
+  else if (pinValuex < 0 && pinValuey == 0) {
+    //    digitalWrite(ydir, HIGH);
+    digitalWrite(xdir, LOW);
+    for (x = 0; x < 800; x++)
+    {
+      //      digitalWrite(xstp, HIGH);
+      digitalWrite(xstp, HIGH);
+      delay(1);
+      digitalWrite(xstp, LOW);
+      digitalWrite(ystp, LOW);
+      delay(1);
+    }
+
+  }
+  else
+  { digitalWrite(ydir, LOW);
+    digitalWrite(xdir, LOW);
+    digitalWrite(xstp, LOW);
+    digitalWrite(ystp, LOW);
+    delay(100);
+  }
+  delay(100);
 
 
 }
